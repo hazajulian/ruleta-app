c662b9d4d5b185fb8b7487ebc8ff80b147e4ce37
hazajulian <hazajulian4@gmail.com>
2026-01-08 16:29:44 -0300

    Fix ScrollToTop

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,29 @@ import { DicePage } from "./pages/DicePage";
 import { RandomNumberPage } from "./pages/RandomNumberPage";
 import { NameDrawPage } from "./pages/NameDrawPage";
 import { Footer } from "./components/Footer/Footer";
+import { ScrollToTop } from "./components/ScrollToTop/ScrollToTop";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <div className="appShell">
-      <Header />
-      <div className="appContent">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/wheel" element={<WheelPage />} />
-        <Route path="/coin" element={<CoinPage />} />     
-        <Route path="/dice" element={<DicePage />} /> 
-        <Route path="/random-number" element={<RandomNumberPage />} />   
-        <Route path="/name-draw" element={<NameDrawPage />} />
-      </Routes>
+      <ScrollToTop />
+
+      <div className="appShell">
+        <Header />
+
+        <div className="appContent">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/wheel" element={<WheelPage />} />
+            <Route path="/coin" element={<CoinPage />} />
+            <Route path="/dice" element={<DicePage />} />
+            <Route path="/random-number" element={<RandomNumberPage />} />
+            <Route path="/name-draw" element={<NameDrawPage />} />
+          </Routes>
+        </div>
+
+        <Footer />
       </div>
-      <Footer />
-    </div>
     </BrowserRouter>
   );
 }
